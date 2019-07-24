9022ab52fd38c4796025fabcbac38b85943d2382
Alexander Fedulin <alexander.fedulin@gmail.com>
2019-07-25 02:54:04 +0300

    Fix build_cloudflare_real_ip_config.sh

--- scripts/build_cloudflare_real_ip_config.sh
+++ scripts/build_cloudflare_real_ip_config.sh
@@ -2,9 +2,9 @@
 
 echo "# Cloudflare" > config/00_real_ip_cloudflare_00.conf
 for i in 'curl https://www.cloudflare.com/ips-v4'; do
-        echo "set_real_ip_from $i;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf
+        echo "set_real_ip_from $i;" >> config/00_real_ip_cloudflare_00.conf
 done
 
 for i in 'curl https://www.cloudflare.com/ips-v6'; do
-        echo "set_real_ip_from $i;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf
+        echo "set_real_ip_from $i;" >> config/00_real_ip_cloudflare_00.conf
 done
